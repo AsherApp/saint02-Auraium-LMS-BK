c5331cfb2ab3a25cbf0194d7c7dfec8d7bffa492
AsherApp <AsherApp@ashercorp.co.uk>
2025-09-17 21:19:26 +0100

    new

--- src/server.ts
+++ src/server.ts
@@ -91,10 +91,8 @@ const allowedOrigins = [
   'https://auraiumlms.vercel.app', // Alternative Vercel URL
   'http://localhost:3000', 
   'http://localhost:3001', 
-  'http://localhost:3002',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001', 
-  'http://127.0.0.1:3002'
 ]
 
 const corsOptions = {
